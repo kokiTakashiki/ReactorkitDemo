6a7f7ee30e19a0ac9d515d219ecea858d84ad691
kokiTakeda <bluewhitered123@gmail.com>
2022-07-05 14:35:44 +0900

    add Demo

---
+++ ReactorKitTests/ReactorKitTests/ReactorKitTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReactorKitTestsApp.swift
+//  ReactorKitTests
+//
+//  Created by 武田孝騎 on 2022/07/05.
+//
+
+import SwiftUI
+
+@main
+struct ReactorKitTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
